f07a92d68ab4b8f054ea5bb09f344057b9a6cfb9
ssit <2sitto@gmail.com>
2022-01-17 16:08:50 +0100

    testing

--- src/main/java/WebService/Warhammer40k/Articles/ArticlesController.java
+++ src/main/java/WebService/Warhammer40k/Articles/ArticlesController.java
@@ -17,7 +17,7 @@ public class ArticlesController {
     }
     
     
-        @CrossOrigin(origins = "http://localhost:3000")
+        @CrossOrigin(origins = "http://127.0.0.1:5500/panel.html")
         @PostMapping
         public Article createArticle(@RequestBody Article art){
             return articlesService.createArticle(art);
